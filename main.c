5c290749bbd314760293889e0841fe2c6fdd01ee
dills122 <Dylan@client-104-39-41-159.mobility.psu.edu>
2016-03-17 11:33:42 -0400

    added process identifiers and a way to split the work between processes
    Added process ID numbers to keep track of individual processes. Also
    added a start and end int value that keeps track of the start and end
    for each process to help divide the work between the processes.

--- main.c
+++ main.c
@@ -6,78 +6,122 @@
 #include <ctype.h>
 
 #define MSGLEN  64
-#define MESSES  10
+#define MESSES  3
+#define ARR 12
 
 int main(){
     int     fd[2];
-    pid_t pid;
+    pid_t   pid;
     int     result;
+
+    int proceID;
+
+    int num[ARR] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+    //Needs to be used in order for the intervals to be determined for the splitting of the file/array
+    int jump = ARR/MESSES;
+    //used to indicate to a specific process what its start and end is in the shared array
+    int start, end;
 
     //Creating a pipe
     result = pipe (fd);
     if (result < 0) {
         //failure in creating a pipe
-        perror("pipe");
+        perror("pipe error\n");
         exit (1);
     }
 
     //Creating a child process
-    pid = fork();
-    if (pid < 0) {
+    for(int i = 0; i < MESSES; i++){
+
+    /* sets the proceID equal to the i in the loop
+        this helps to distinguish the individual processes
+    */
+    proceID = i;
+
+    //checks if it is the first child process
+    if (proceID == 0) {
+        start = 0;
+        end = jump;
+
+    }
+    // sets all of the next processes start and end points
+    else {
+        start = end;
+        end += jump;
+    }
+
+	if ((pid = fork()) < 0) {
          //failure in creating a child
-         perror ("fork");
+         perror ("fork error\n");
          exit(2);
+        }
+	if(pid == 0)
+	   break;
     }
 
     if (pid == 0) {
-        //Child process
+        // ACTUALLY CHILD PROCESS
          char message[MSGLEN];
 
-          for(int i = 0; i < MESSES; i++) {
-                    //Clearing the message
-                    memset (message, 0, sizeof(message));
-                    printf ("Enter a message: ");
-                    //scanf ("%s",message);
+         for (int i = 0; i < MESSES; i++) {
 
-                    fgets (message, 1024, stdin);
+            if (proceID == i) {
 
-                    //Writing message to the pipe
-                    write(fd[1], message, strlen(message));
+                printf("The process ID: %d\n", proceID);
+                printf("The start is %d and end is %d\n", start, end);
+
+                for (int k = start; k < end; k++) {
+                    printf("The number is %d in process %d\n", num[k], proceID);
+                }
+
             }
+
+         }
+
+            //Clearing the message
+            memset (message, 0, sizeof(message));
+
+            //fgets (message, 1024, stdin);
+            close(fd[0]);
+
+            //Writing message to the pipe
+            write(fd[1], message, strlen(message));
+
             close(fd[1]);
+            close(fd[0]);
+
             exit (0);
     }
     else {
         //Parent Process
-        wait(NULL);
-         char message[MSGLEN];
+
+        char message[MSGLEN];
+        char *ptr;
+        long wc;
+        close(fd[1]);
 
          while (1) {
                     //Clearing the message buffer
-                    memset (message, 0, sizeof(message));
+            memset (message, 0, sizeof(message));
 
                     //Reading message from the pipe
 
-                    read (fd[0], message, sizeof(message));
-                    printf("Message entered %s\n",message);
-                    /*
-                    Message entered needs to be in the format of number first space then string for it to work
-                    */
+            if(read(fd[0], message, sizeof(message)) == 0)
+                exit(0);
+                printf("Message entered %s\n",message);
+                /*
+                Message entered needs to be in the format of number first space then string for it to work
+                */
 
-                    char *ptr;
-                    long wc;
-
-                    wc = strtol(message, &ptr, 10);
-                    printf("The number(unsigned long integer) is %ld\n", wc);
-                    printf("String part is %s", ptr);
-
-                    wc = 0;
-                    ptr = "";
-
+                wc = 0;
+                wc = strtol(message, &ptr, 10);
+                printf("The number(unsigned long integer) is %ld\n", wc);
+                printf("String part is %s", ptr);
             }
-
-            exit(0);
+	    close(fd[0]);
+	    wait(NULL);
+        exit(0);
      }
      return 0;
 }
-
